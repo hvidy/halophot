e177314a8c2de9a12d7a6717c9bb05c924ee1019
Benjamin Pope <benjamin.pope@nyu.edu>
2018-07-18 11:16:41 +0200

    requirements

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 long_description = ''
 
 setup(name='halophot',
-      version='0.6.0',
+      version='0.6.1.2',
       description='K2 halo photometry with total variation.',
       long_description=long_description,
       author='Tim White and Benjamin Pope',
@@ -15,7 +15,7 @@ setup(name='halophot',
       package_dir={'halophot':'src'},
       scripts=['bin/halo'],
       packages=['halophot'],
-      install_requires=["numpy", "astropy", "scipy","autograd"],
+      install_requires=["numpy", "astropy", "scipy","autograd","future==0.16.0"],
       license='GPLv3',
       classifiers=[
           "Topic :: Scientific/Engineering",
